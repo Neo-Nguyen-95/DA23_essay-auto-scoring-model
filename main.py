1ce8939af8fca7e5ffbf722cb3ca58ba1b228506
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-06-23 10:11:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #%% LIB
 import pandas as pd
 pd.set_option('display.max_columns', None)
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 import re
 import nltk
@@ -15,9 +17,24 @@ english_vocab = set(words.words())
 from spellchecker import SpellChecker
 from collections import Counter
 
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression, Ridge
+from sklearn.svm import LinearSVC
+
+from transformers import (
+    BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+    )
+from datasets import Dataset
+
+
+
 df = pd.read_csv('data/train.csv')
 
-#%%skip_if True EDA 1
+#%% EDA 1
 
 # df.head()
 
@@ -77,6 +94,8 @@ df = pd.read_csv('data/train.csv')
 # corrected_spell = [spell.correction(word) for word in misspelled]  # Undetected word might be object/location name, I can filter it if SpellChecker cannot detect it.
 # count_misspelled = len([word for word in corrected_spell if word])
 
+### Stop-word removal are not applied in this case-study
+
 #%% EDA 2
 
 stop_words = set(stopwords.words('english'))
@@ -113,26 +132,153 @@ def describe_text(row):
         count_misspelled / count_text if count_text > 0 else 0
         )
     
+    # Collection of words (remove stop words)
+    words = [word for word in lemmatized_text if word not in stop_words and word not in misspelled]
+    
+    # Result
     row['text_length'] = count_text
     row['stopword_percent'] = percent_stopwords_text
     row['misspelled_percent'] = percent_misspelled
+    row['word_collection'] = words
     
     return row
 
 df = df.apply(describe_text, axis=1)
 
+# # Average table
+# df.groupby('score')[['text_length', 'stopword_percent', 'misspelled_percent']].mean()
+
+# # Text length plot
+# sns.violinplot(
+#     data=df,
+#     x='text_length',
+#     y='score',
+#     orient='h',
+#     split=True
+#     )
+# plt.title('Text\'s length distribution corresponding to score')
+# plt.xlim(0, 1400)
+# plt.show()
+
+# # Stopword percentage plot
+# sns.violinplot(
+#     data=df,
+#     x='stopword_percent',
+#     y='score',
+#     orient='h',
+#     split=True
+#     )
+# plt.title('Distribution of percentage of stop words corresponding to score')
+# plt.show()
+
+# # Misspelled percentage plot
+# sns.violinplot(
+#     data=df,
+#     x='misspelled_percent',
+#     y='score',
+#     orient='h',
+#     split=True
+#     )
+# plt.title('Distribution of percentage of misspelled words  corresponding to score')
+# plt.xlim(0, 0.1)
+# plt.show()
+
+#%% SCORING BY TEXT CLASSIFICATION
+# # Classification with Naive Bayes, Logistic Regression, SVM
+
+# ### Multinomial NB
+# texts = [' '.join(words) for words in df['word_collection'].to_list()]
+# labels = df['score'].to_list()
+
+# X_train, X_val, y_train, y_val = train_test_split(
+#     texts, labels, test_size=0.2, random_state=42, stratify=labels
+#     )
+
+# model_NB = make_pipeline(TfidfVectorizer(), MultinomialNB())
+# model_NB.fit(X_train, y_train)
+# prediction = model_NB.predict(X_val)
+# print(f'Accuracy of Naive Bayes Classifier: {accuracy_score(y_val, prediction):.2f}')
 
 
+# model_LR = make_pipeline(
+#     TfidfVectorizer(), LogisticRegression(max_iter=500)
+#     )
+# model_LR.fit(X_train, y_train)
+# prediction = model_LR.predict(X_val)
+# print(f'Accuracy of Logistic Regression: {accuracy_score(y_val, prediction):.2f}')
 
-### TF-IDF (Term Frequency–Inverse Document Frequency)
+# model_SVM = make_pipeline(TfidfVectorizer(), LinearSVC())
+# model_SVM.fit(X_train, y_train)
+# prediction = model_SVM.predict(X_val)
+# print(f'Accuracy of SVM: {accuracy_score(y_val, prediction):.2f}')
 
 
-### N-gram analysis
+# X_train, X_val, y_train, y_val = train_test_split(
+#     df[['text_length', 'misspelled_percent']], df['score'], test_size=0.2, random_state=42, stratify=labels
+#     )
+
+# model_ridge = Ridge(alpha=1)
+# model_ridge.fit(X_train, y_train)
+# y_pred = model_ridge.predict(X_val)
+# y_pred_rounded = [int(round(y, 0)) for y in y_pred]
+# print(f'Accuracy of Ridge: {accuracy_score(y_val, y_pred_rounded):.2f}')
+
+#%% SCORING BY FINE-TUNING BERT
+
+# Load tokenizer
+tokenizer_bert = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Convert the dataset
+dataset = Dataset.from_pandas(df[['full_text', 'score']])
+dataset = dataset.rename_column('score', 'label')
+
+# Tokenize
+def tokenizer_func(df):
+    return tokenizer_bert(
+        df['full_text'], truncation=True, padding='max_length', max_length=512
+        )
+
+tokenized_dataset = dataset.map(tokenizer_func, batch_size=True)
+
+# Train - test split
+tokenized_dataset = tokenized_dataset.train_test_split(test_size=0.2)
+train_dataset = tokenized_dataset['train']
+eval_dataset = tokenized_dataset['test']
+
+# Load model
+model = BertForSequenceClassification.from_pretrained(
+    'bert-base-uncased', num_labels=6
+    )  # scores 1–6
 
 
+# Training setup
+training_args = TrainingArguments(
+    output_dir="./bert-essay",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,  # split your data for real use
+)
+
+# Step 5: Train
+trainer.train()
+
+# Step 6: Predict
+preds = trainer.predict(eval_dataset)
+pred_labels = np.argmax(preds.predictions, axis=1)
+true_labels = eval_dataset['label']
+
+print(f"Accuracy of BERT: {accuracy_score(true_labels, pred_labels):.2f}")
 
 
-### Stop-word removal are not applied in this case-study
 
 
 
